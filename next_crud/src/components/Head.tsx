013e6ed6023c2e7ebbc4970cc898a2989bd2bcd1
Vanderllee <vanderllee@gmail.com>
2021-08-01 12:32:45 -0300

    created aplication Head

---
+++ next_crud/src/components/Head.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react"
+
+type HeadProps = {
+    children: ReactNode;
+}
+
+export default function Head({children}: HeadProps) {
+    return (
+        <div className="flex flex-col justify-center">
+            <h1 className="px-5 py-2 text-2xl">{children}</h1>
+            <hr className="border-2 border-purple-500"/>
+        </div>
+    )
+}
